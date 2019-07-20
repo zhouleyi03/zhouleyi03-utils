c49ac2586a1227151eef7208c26904e7e508ca45
zhouleyi03 <zhouleyi2003@outlook.com>
2019-07-20 14:07:25 +0800

    sunday(string matching)

--- zhouleyi03-utils/main.cpp
+++ zhouleyi03-utils/main.cpp
@@ -1,11 +1,15 @@
 #include "zutils.h"
 #include <iostream>
+#include <Windows.h>
 
 int main()
 {
-	std::string s_t("helloworld");
-	std::string s_p("llowo");
-	bool result = zutils::string::kmp(s_p, s_t);
+	std::string s_t("HelloWorldsssssaaskdhjklasfkhkasdjklajsdjjalsflajldksh;lalwjdsssssssssssssssssssssssssssssssssss");
+	std::string s_p("oWo");
+	DWORD time1 = GetTickCount64();
+	bool result = zutils::string::sunday(s_p, s_t);
+	std::cout << GetTickCount64() - time1 << std::endl;
+
 	if (result)
 		std::cout << "Pattern occurs!" << std::endl;
 	else
--- zhouleyi03-utils/zutils-string.hpp
+++ zhouleyi03-utils/zutils-string.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <string>
+#include <array>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 namespace zutils
 {
@@ -15,7 +17,7 @@ namespace zutils
 		*/
 		bool kmp(const std::string& pattern,const std::string& text)
 		{
-			std::vector<int> pi(pattern.size(), 0);
+			std::vector<std::size_t> pi(pattern.size(), 0);
 			std::size_t k = 0;
 			std::size_t pattern_length = pattern.size();
 
@@ -36,6 +38,44 @@ namespace zutils
 					++k;
 				else
 					k = pi[k + 1];
+				if (k == pattern_length - 1)
+					return true;
+			}
+			return false;
+		}
+
+		/*
+
+			-最后修改于：2019/7/20 13时
+			描述：使用Sunday算法搜索指定字符串，返回true则搜索到，反之则未搜索到。
+
+		*/
+		bool sunday(const std::string& pattern, const std::string& text)
+		{
+			std::size_t k = 0;
+			std::size_t pattern_length = pattern.size();
+			std::vector<std::size_t> pi(58, pattern_length);
+
+			for (std::size_t i = 0; i < pattern.size(); i++)
+				pi[pattern[i] - 65] = std::min(pi[pattern[i] - 65], pattern_length - i - 1);
+
+			for (std::size_t j = 0; j < text.size();)
+			{
+				if (text[j] != pattern[k])
+				{
+					if (pi[text[j + pattern_length] - 65] != pattern_length)
+					{
+						j += (pi[text[j + pattern_length] - 65] + 1);
+					}
+					else
+						j += (pattern_length + 1);
+					k = 0;
+				}
+				else
+				{
+					++k;
+					++j;
+				}
 				if (k == pattern_length - 1)
 					return true;
 			}
