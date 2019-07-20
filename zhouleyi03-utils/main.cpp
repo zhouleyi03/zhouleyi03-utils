45fdea3434a38a893448cc1d1b1ed1b7a4a65cbd
zhouleyi03 <zhouleyi2003@outlook.com>
2019-07-20 15:14:44 +0800

    fix

--- zhouleyi03-utils/main.cpp
+++ zhouleyi03-utils/main.cpp
@@ -1,18 +1,11 @@
 #include "zutils.h"
 #include <iostream>
-#include <Windows.h>
 
 int main()
 {
-	std::string s_t("HelloWorldsssssaaskdhjklasfkhkasdjklajsdjjalsflajldksh;lalwjdsssssssssssssssssssssssssssssssssss");
-	std::string s_p("oWo");
-	DWORD time1 = GetTickCount64();
-	bool result = zutils::string::sunday(s_p, s_t);
-	std::cout << GetTickCount64() - time1 << std::endl;
-
-	if (result)
-		std::cout << "Pattern occurs!" << std::endl;
-	else
-		std::cout << "Pattern doesn't occur." << std::endl;
+	std::string s_t("abababaca");
+	std::string s_p("ababaca");
+	int result = zutils::string::kmp(s_p, s_t);
+	std::cout << result<<" time(s)";
 	return 0;
 }
--- zhouleyi03-utils/zutils-string.hpp
+++ zhouleyi03-utils/zutils-string.hpp
@@ -12,19 +12,20 @@ namespace zutils
 		/*
 		
 			-最后修改于：2019/7/20 11时
-			描述：使用KMP算法搜索指定字符串，返回true则搜索到，反之则未搜索到。
+			描述：使用KMP算法搜索指定字符串，返回搜索到的次数。
 
 		*/
-		bool kmp(const std::string& pattern,const std::string& text)
+		int kmp(const std::string& pattern,const std::string& text)
 		{
 			std::vector<std::size_t> pi(pattern.size(), 0);
 			std::size_t k = 0;
+			std::size_t searched_times = 0;
 			std::size_t pattern_length = pattern.size();
 
 			pi.push_back(0);
 			for (std::size_t i = 1; i < pattern_length; i++)
 			{
-				while (k > 0 && pattern[k] != pattern[i])
+				while (k != 0 && pattern[k] != pattern[i])
 					k = pi[k - 1];
 				if (pattern[k] == pattern[i])
 					++k;
@@ -34,14 +35,17 @@ namespace zutils
 			k = 0;
 			for (std::size_t j = 0; j < text.size(); j++)
 			{
-				if (text[j] == pattern[k])
+				while (k != 0 && pattern[k] != text[j])
+					k = pi[k - 1];
+				if (pattern[k] == text[j])
 					++k;
-				else
-					k = pi[k + 1];
-				if (k == pattern_length - 1)
-					return true;
+				if (k == pattern_length)
+				{
+					++searched_times;
+					k = 0;
+				}
 			}
-			return false;
+			return searched_times;
 		}
 
 		/*
